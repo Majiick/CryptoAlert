541a41fd398b02ebc19f8805e4f25f44ebba028d
root <root@debian-s-2vcpu-4gb-lon1-01.localdomain>
2018-10-18 19:22:32 +0000

    Added data api skeleton

---
+++ collector/data_api.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+# Example structure
+# pair btcusd:
+#   exchange poloniex:
+#     data source cryptowatch
+#     data source poloniex
+# This means that btcusd is traded on poloniex. To get the price data we can either use poloniex api or cryptowatch api.
+
+class Pair:
+    def __init__(self, pair: str):
+        self.pair = pair
+
+
+class Exchange:
+    def __init__(self, name: str):
+        self.name = name
+
+
+class DataAPI(ABC):
+    @abstractmethod
+    def get_last_price(self, pair: Pair, exchange: Exchange) -> float:
+        pass
