c16513250241983a64f62074d7fb69d9f6d52498
root <root@debian-s-2vcpu-4gb-lon1-01.localdomain>
2018-10-18 22:08:54 +0000

    Cleaning up

--- collector/cryptowatch.py
+++
@@ -1,8 +0,0 @@
-from data_api import DataAPI, Pair, Exchange
-
-class Cryptowatch(DataAPI):
-    def get_last_price(pair: Pair, exchange: Exchange) -> float:
-        pass
-
-C = Cryptowatch()
-C.get_last_price(Pair('btcusd'))
