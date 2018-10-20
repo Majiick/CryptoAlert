dc1200246a6f9cb53668d003781a83c0b484fdf8
root <root@debian-s-2vcpu-4gb-lon1-01.localdomain>
2018-10-20 17:23:01 +0000

    Changed cryptowatch get ohlc

--- collector/data_api/cryptowatch.py
+++ collector/data_api/cryptowatch.py
@@ -13,6 +13,10 @@ class Cryptowatch(DataAPI):
 
     @staticmethod
     def get_ohlc(pair: Pair, exchange: Exchange, start_time: int, end_time: int, periods: List[int]):
+        '''
+        Cryptowatch limits historical data.
+        For the 60 tick you can't go back more than 30000 seconds or so.
+        '''
         assert(end_time > start_time)
         assert(periods)
         supported_periods = [60, 180, 300, 900, 1800, 3600, 7200, 14400, 21600, 43200, 86400, 259200, 604800]
@@ -46,33 +50,6 @@ class Cryptowatch(DataAPI):
                 else:
                     print('No result at earliest_time {}'.format(earliest_time))
                     break
-
-        '''
-        max_limit_per_period = 497
-        for cur_period in periods:
-            max_time_frame = max_limit_per_period * cur_period
-            time_frames_to_queury: List[Tuple[int, int]] = []  # Stores time frames which are (start, end).
-            cur_time = end_time
-            while cur_time > start_time:
-                time_frames_to_queury.append((cur_time - max_time_frame, cur_time))
-                cur_time = cur_time - max_time_frame
-
-            for time_frame in time_frames_to_queury:
-                print('\n')
-                print('cur_period: {}'.format(cur_period))
-                print('time_frame: {}'.format(time_frame))
-                params = {'after': time_frame[0] - 1, 'before': time_frame[1] + 1}
-                r = requests.get('https://api.cryptowat.ch/markets/{}/{}/ohlc'.format(exchange.name, pair.pair),
-                                 params=params)
-                json_data = json.loads(r.content)
-                print(json_data['allowance'])
-
-                for period in periods:
-                    if json_data['result'][str(period)]:
-                        print(len(json_data['result'][str(period)]))
-                    else:
-                        print('Missed period {} for timeframe {}'.format(period, time_frame))
-        '''
 
 
     @staticmethod
