2daa449429eeb140d3d9559854508bdbc68193c2
root <root@debian-s-2vcpu-4gb-lon1-01.localdomain>
2018-11-12 13:56:55 +0000

    Added socketio message propagation of alerts

--- alert_generation/socketio_server/socketio_server.py
+++ alert_generation/socketio_server/socketio_server.py
@@ -1,14 +1,11 @@
+import eventlet
+# eventlet.monkey_patch()
 import socketio
 import zmq
-import eventlet
+import threading
 from aiohttp import web
 
 print("Started socketio server")
-
-context = zmq.Context()
-sub_socket = context.socket(zmq.SUB)  # Subs to alert_generation pub socket
-sub_socket.connect('tcp://alert_generation:28000')
-sub_socket.setsockopt_string(zmq.SUBSCRIBE, '')
 
 app = web.Application()
 sio = socketio.Server(async_mode='eventlet')
@@ -16,10 +13,30 @@ sio = socketio.Server(async_mode='eventlet')
 @sio.on('connect', namespace='/price_update')
 def connect(sid, environ):
     print("connect ", sid)
+    sio.emit('price_update', {'data': 'test'}, namespace='/price_update')
+
+
+def listen_to_alert_pub():
+    context = zmq.Context()
+    sub_socket = context.socket(zmq.SUB)  # Subs to alert_generation pub socket
+    sub_socket.connect('tcp://alert_generation:28000')
+    sub_socket.setsockopt_string(zmq.SUBSCRIBE, '')
+
+    while True:
+        json = sub_socket.recv_json()
+        print(json)
+        sio.emit('price_update', {'data': json}, namespace='/price_update')
+
+
+def serve_app(_sio, _app):
+    app = socketio.Middleware(_sio, _app)
+    eventlet.wsgi.server(eventlet.listen(('', 8333)), app)
+    
+
+t = threading.Thread(target=listen_to_alert_pub)
+t.start()
 
 app = socketio.Middleware(sio, app)
+# sio.start_background_task(target=listen_to_alert_pub)
 eventlet.wsgi.server(eventlet.listen(('', 8333)), app)
 
-while True:
-    json = sub_socket.recv_json()
-    print(json)
