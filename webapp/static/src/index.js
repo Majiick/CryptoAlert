84d91c799b8144bfcd5ae7de535340b3f5ccd975
root <root@debian-s-2vcpu-4gb-lon1-01.localdomain>
2018-11-16 15:54:27 +0000

    Simple React Layout

--- webapp/static/src/index.js
+++ webapp/static/src/index.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import 'semantic-ui-css/semantic.min.css';
-import { Button, Icon, Label, Menu } from 'semantic-ui-react'
+import { Button, Icon, Label, Menu, List, Header, Container, Divider } from 'semantic-ui-react'
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -24,21 +24,25 @@ socket.on('alert', function (data) {
 });
 
 class AlertsButton extends React.Component {
-	  render() {
-		  return (
-			  <div>
-	    		  	<Button as='div' labelPosition='right'>
-	                  		<Button color='red'>
-	                  			<Icon name='exclamation triangle' />
-	                  			Alerts
-	                  		</Button>
-	      				<Label as='a' basic color='red' pointing='left'>
-	        				3
-	     				</Label>
-			  	</Button>
-	  		  </div>
-		  	);
-	  }
+    constructor(props) {
+        super(props);
+      }
+
+    render() {
+      return (
+          <div>
+                <Button as='div' labelPosition='right'>
+                    <Button color='red'>
+                        <Icon name='exclamation triangle' />
+                        Alerts
+                    </Button>
+                    <Label as='a' basic color='red' pointing='left'>
+                        {this.props.number_alerts}
+                    </Label>
+            </Button>
+          </div>
+        );
+    }
 }
 
 
@@ -52,15 +56,65 @@ class TopMenu extends React.Component {
         return (
           <Menu Inverted>
             <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-            <Menu.Item name='alerts' active={activeItem === 'messages'} onClick={this.handleItemClick} />
-            <Menu.Menu position='right'> <AlertsButton/> </Menu.Menu>
+            <Menu.Item name='alerts' active={activeItem === 'alerts'} onClick={this.handleItemClick} />
+            <Menu.Menu position='right'>
+                <AlertsButton number_alerts={this.props.number_alerts}/>
+            </Menu.Menu>
 
           </Menu>
         );
     }
 }
 
-ReactDOM.render(<TopMenu/>, document.getElementById('root'));
+
+class Alerts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {alert_notification: [], subscribed_alerts: []};
+        this.state.alert_notification.push(<List.Item>Test Alert Notification</List.Item>);
+        this.state.subscribed_alerts.push(<List.Item>Test My Alert</List.Item>);
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+            <Container>
+                <Header as='h2'>Alert Notifications</Header>
+                <List>
+                    {this.state.alert_notification}
+                </List>
+            </Container>
+
+            <Divider />
+
+            <Container>
+                <Header as='h2'>My Alerts</Header>
+                <List>
+                    {this.state.subscribed_alerts}
+                </List>
+            </Container>
+            </React.Fragment>
+        );
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {number_alerts: 0}
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <TopMenu number_alerts={ this.state.number_alerts} />
+                <Alerts />
+            </React.Fragment>
+        );
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 //const element = <h1>Hello, world</h1>;
 //ReactDOM.render(element, document.getElementById('root'));
 //document.body.appendChild(ButtonExampleLabeledBasic());
