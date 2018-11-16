a75ed460ed1229393ac1e2504cb454770cc859f2
root <root@debian-s-2vcpu-4gb-lon1-01.localdomain>
2018-11-16 14:08:48 +0000

    Added semantics ui and made menu work

--- webapp/static/src/index.js
+++ webapp/static/src/index.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import 'semantic-ui-css/semantic.min.css';
-import { Button, Icon, Label } from 'semantic-ui-react'
+import { Button, Icon, Label, Menu } from 'semantic-ui-react'
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -23,38 +23,44 @@ socket.on('alert', function (data) {
     console.log(data);
 });
 
-const ButtonExampleLabeledBasic = () => (
-	  <div>
-	    <Button as='div' labelPosition='right'>
-	      <Button color='red'>
-	        <Icon name='heart' />
-	        Like
-	      </Button>
-	      <Label as='a' basic color='red' pointing='left'>
-	        2,048
-	      </Label>
-	    </Button>
-	    <Button as='div' labelPosition='right'>
-	      <Button basic color='blue'>
-	        <Icon name='fork' />
-	        Fork
-	      </Button>
-	      <Label as='a' basic color='blue' pointing='left'>
-	        2,048
-	      </Label>
-	    </Button>
-	  </div>
-)
-
-
-class ShoppingList extends React.Component {
+class AlertsButton extends React.Component {
 	  render() {
-		      return ButtonExampleLabeledBasic(); 
-		    }
+		  return (
+			  <div>
+	    		  	<Button as='div' labelPosition='right'>
+	                  		<Button color='red'>
+	                  			<Icon name='exclamation triangle' />
+	                  			Alerts
+	                  		</Button>
+	      				<Label as='a' basic color='red' pointing='left'>
+	        				3
+	     				</Label>
+			  	</Button>
+	  		  </div>
+		  	);
+	  }
 }
 
 
-ReactDOM.render(<ShoppingList name="Gabby" />, document.getElementById('root'));
+class TopMenu extends React.Component {
+    state = {activeItem: 'home'}
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+    render() {
+        const { activeItem } = this.state;
+
+        return (
+          <Menu Inverted>
+            <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+            <Menu.Item name='alerts' active={activeItem === 'messages'} onClick={this.handleItemClick} />
+            <Menu.Menu position='right'> <AlertsButton/> </Menu.Menu>
+
+          </Menu>
+        );
+    }
+}
+
+ReactDOM.render(<TopMenu/>, document.getElementById('root'));
 //const element = <h1>Hello, world</h1>;
 //ReactDOM.render(element, document.getElementById('root'));
 //document.body.appendChild(ButtonExampleLabeledBasic());
